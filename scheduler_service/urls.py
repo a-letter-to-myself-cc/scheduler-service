9e3e24a1abce40868cb23f61ec11a70e07544e14
Yeongeunn <nye0817@gmail.com>
2025-06-06 20:33:14 +0900

    feat: 헬스체크 urls 수정

--- scheduler_service/urls.py
+++ scheduler_service/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from schedule.views import health_check
 
 urlpatterns = [
     path("", include("schedule.urls")), # 내부 API만 씀
+    path('health/', health_check), 
 ]
