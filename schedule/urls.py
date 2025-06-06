100380164f4a53e12028afc691a9c5c04a50cb39
Yeongeunn <nye0817@gmail.com>
2025-06-06 20:06:12 +0900

    .dockerignore 수정

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import celery_status
+from .views import *
+from . import views
+
 
 urlpatterns = [
     path('scheduler/status/', celery_status, name='celery_status'),
